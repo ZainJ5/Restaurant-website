1b11b9f55f6896235745b3caed3ce75bacdda702
ZainJ5 <154011623+ZainJ5@users.noreply.github.com>
2025-02-17 12:13:31 +0500

    Updated Bannar

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -1,12 +1,10 @@
 export default function Banner() {
   return (
-    <div
-      className="w-[85%] m-auto rounded-md h-48"
-      style={{
-        backgroundImage: "url('/bannar.webp')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    />
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-16">
+      <div
+        className="aspect-[1500/287] rounded-md bg-cover bg-center"
+        style={{ backgroundImage: "url('/bannar.webp')" }}
+      />
+    </div>
   )
 }
